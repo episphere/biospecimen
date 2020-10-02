937d864b83d7353acb9643978197463ab1e98358
bhaumik55231 <bhaumik55231@gmail.com>
2020-10-02 09:31:30 -0400

    Adding dashboard selection dropdown on welcome screen

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,4 +1,4 @@
-import { validateUser, siteFullNames, showAnimation, hideAnimation } from "./../shared.js";
+import { validateUser, siteFullNames, showAnimation, hideAnimation, errorMessage, removeAllErrors } from "./../shared.js";
 import { userDashboard } from "./dashboard.js";
 import { nonUserNavBar } from './../navbar.js'
 
@@ -25,6 +25,9 @@ const welcomeScreenTemplate = (name, data, auth, route) => {
         <div class="row welcome-screen-div">
             <div class="col">IHCS: ${siteFullNames[data.siteAcronym]}</div>
         </div>
+        <div class="row welcome-screen-div">
+            <div class="col"><label for="dashboardSelection" class="col-form-label">Select dashboard to use </label><select required class="col form-control" id="dashboardSelection"><option value="">-- Select dashbaord --</option><option>Clinical Dashboard</option><option>Research Dashboard</option></select></div>
+        </div>
         <div class="row welcome-screen-btn-div">
             <div class="col"><button class="btn btn-outline-primary" id="btnParticipantSearch">Participant Search</button></div>
             <div class="col"><button class="btn btn-outline-secondary" id="btnSpecimenSearch">Specimen Search</button></div>
@@ -36,11 +39,24 @@ const welcomeScreenTemplate = (name, data, auth, route) => {
     document.getElementById('contentHeader').innerHTML = '';
 
     document.getElementById('btnParticipantSearch').addEventListener('click', () => {
+        removeAllErrors();
+        const selection = document.getElementById('dashboardSelection');
+        if(!selection.value) {
+            errorMessage('dashboardSelection', 'Please select Clinical or Research dashboard', true);
+            return;
+        }
         location.hash = '#dashboard';
     });
 
     document.getElementById('btnSpecimenSearch').addEventListener('click', () => {
+        removeAllErrors();
+        const selection = document.getElementById('dashboardSelection');
+        if(!selection.value) {
+            errorMessage('dashboardSelection', 'Please select Clinical or Research dashboard', true);
+            return;
+        }
         window.history.replaceState({},'', './#dashboard');
+        
         userDashboard(auth, route, true);
     });
 }
