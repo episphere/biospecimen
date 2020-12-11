6e3cfee9282736a1ab78b021acc85ea1193d65ba
bhaumik55231 <bhaumik55231@gmail.com>
2020-12-11 15:34:11 -0500

    minor update

--- src/shared.js
+++ src/shared.js
@@ -581,7 +581,7 @@ export const addEventBarCodeScanner = (id, start, end) => {
                     disableInput('enterSpecimenID2', true);
                     addEventClearScannedBarcode('clearScanSpecimenID');
                 }
-                document.getElementById(elementID).value = start && end ? result.codeResult.code.substr(start, end) : result.codeResult.code;
+                document.getElementById(elementID).value = start !== undefined && end !== undefined ? result.codeResult.code.substr(start, end) : result.codeResult.code;
                 Quagga.stop();
                 document.querySelector('[data-dismiss="modal"]').click();
             }
