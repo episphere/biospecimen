4b079a47664b0db396d2731fe19a79de7a3ecc30
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2021-08-23 22:22:02 -0400

    git merge

--- index.js
+++ index.js
@@ -10,28 +10,8 @@ import { kitAssemblyScreen } from "./src/pages/homeCollection/kitAssembly.js";
 import { printAddressesScreen } from "./src/pages/homeCollection/printAddresses.js";
 import { assignedScreen } from "./src/pages/homeCollection/assigned.js";
 import { addressesPrintedScreen } from "./src/pages/homeCollection/addressPrinted.js";
-<<<<<<< HEAD
-import { kitShipmentScreen } from "./src/pages/homeCollection/kitShipment.js";
-
-let auth = '';
-
-window.onload = () => {
-    if('serviceWorker' in navigator){
-        try {
-            navigator.serviceWorker.register('./serviceWorker.js');
-        }
-        catch (error) {
-            console.log(error);
-        }
-    };
-    !firebase.apps.length ? firebase.initializeApp(firebaseConfig()) : firebase.app();
-    auth = firebase.auth();
-   // location.hash = '#welcome';
-    manageRoutes();
-}
-=======
 import { allParticipantsScreen } from "./src/pages/homeCollection/allParticipants.js";
-import { shippedScreen } from "./src/pages/homeCollection/kitShipment.js";
+import { kitShipmentScreen } from "./src/pages/homeCollection/kitShipment.js";
 
 let auth = "";
 
@@ -51,35 +31,12 @@ window.onload = () => {
   // location.hash = '#welcome';
   manageRoutes();
 };
->>>>>>> dev
 
 window.onhashchange = () => {
   manageRoutes();
 };
 
 const manageRoutes = async () => {
-<<<<<<< HEAD
-    const route =  window.location.hash || '#';
-    if(await userLoggedIn()){
-        if (route === '#dashboard') userDashboard(auth, route);
-        else if(route === "#shipping") shippingDashboard(auth,route);
-        else if (route === '#welcome') welcomeScreen(auth, route);
-        else if (route === '#bptl') bptlScreen(auth, route);
-        else if (route === '#kitassembly') kitAssemblyScreen(auth, route);
-        else if (route === '#participantselection') printAddressesScreen(auth, route);
-        else if (route === '#addressPrinted') addressesPrintedScreen(auth, route);
-        else if (route === '#assigned') assignedScreen(auth, route);
-        else if (route === '#kitshipment') kitShipmentScreen(auth, route);
-        else if (route === '#manage_users') manageUsers(auth, route);
-        else if (route === '#sign_out') signOut();
-        else if (route === '#reports') reportsQuery(auth, route);
-        else window.location.hash = '#welcome';
-    }else{
-        if(route === '#') signIn();
-        else window.location.hash = '#';
-    }
-}
-=======
   const route = window.location.hash || "#";
   if (await userLoggedIn()) {
     if (route === "#dashboard") userDashboard(auth, route);
@@ -92,7 +49,7 @@ const manageRoutes = async () => {
     else if (route === "#allParticipants") allParticipantsScreen(auth, route);
     else if (route === "#addressPrinted") addressesPrintedScreen(auth, route);
     else if (route === "#assigned") assignedScreen(auth, route);
-    else if (route === "#shipped") shippedScreen(auth, route);
+    else if (route === "#shipped") kitShipmentScreen(auth, route);
     else if (route === "#manage_users") manageUsers(auth, route);
     else if (route === "#sign_out") signOut();
     else if (route === "#reports") reportsQuery(auth, route);
@@ -102,7 +59,6 @@ const manageRoutes = async () => {
     else window.location.hash = "#";
   }
 };
->>>>>>> dev
 
 const userLoggedIn = () => {
   return new Promise((resolve, reject) => {
