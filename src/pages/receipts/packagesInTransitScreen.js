320de32e53e37a993a0fd38f8ee79d21d8106984
Gbarra9 <gebarra9@gmail.com>
2021-08-30 15:20:17 -0400

    import getAllBoxes from shared.js to fetch all packages, add packages in transit rows to table, add bootstrap modal to manifest button(no content, only skeleton)

--- src/pages/receipts/packagesInTransitScreen.js
+++ src/pages/receipts/packagesInTransitScreen.js
@@ -1,3 +1,5 @@
+import { showAnimation, hideAnimation, getAllBoxes } from "../../shared.js";
+import fieldToConceptIdMapping from "../../fieldToConceptIdMapping.js";
 import { receiptsNavbar } from "./receiptsNavbar.js";
 import { nonUserNavBar, unAuthorizedUser } from "./../../navbar.js";
 
@@ -9,6 +11,9 @@ export const packagesInTransitFromSitesScreen = async (auth, route) => {
 };
 
 const packagesInTransitFromSitesTemplate = async (username, auth, route) => {
+  showAnimation();
+  const response = await getAllBoxes();
+  hideAnimation();
   let template = "";
 
   template += receiptsNavbar();
@@ -30,12 +35,77 @@ const packagesInTransitFromSitesTemplate = async (username, auth, route) => {
                                     </tr>
                                 </thead>   
                                 <tbody id="contentBodyPackagesInTransit">
+                                    ${createPackagesInTransitRows(response)}
                                 </tbody>
                         </table>
                     </div>
                 </div>`;
 
+  template += `<div class="modal fade" id="manifestModal" tabindex="-1" aria-labelledby="manifestModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="manifestModalLabel">Manifest Modal</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
   document.getElementById("contentBody").innerHTML = template;
   document.getElementById("navbarNavAltMarkup").innerHTML =
     nonUserNavBar(username);
+};
+
+const createPackagesInTransitRows = (response) => {
+  let template = "";
+  let bagId = "";
+
+  try {
+    if (response.code !== 200) {
+      throw "status code not 200!";
+    } else {
+      const allBoxes = response.data;
+      allBoxes.forEach((i) => {
+        // console.log(i[fieldToConceptIdMapping.shippingShipDate]);
+        // console.log(i[fieldToConceptIdMapping.shippingTrackingNumber]);
+        // console.log(i[fieldToConceptIdMapping.shippingSite]);
+        // console.log(i);
+        // console.log(i.bags);
+        // // returns an array of a given object's own enumerable property names
+        // console.log(Object.keys(i.bags).length);
+        template += `
+                      <tr class="packageInTransitRow">
+                      <td>${
+                        i[fieldToConceptIdMapping.shippingShipDate]
+                          ? i[fieldToConceptIdMapping.shippingShipDate]
+                          : "N/A"
+                      }</td>
+                      <td>${
+                        i[fieldToConceptIdMapping.shippingTrackingNumber]
+                          ? i[fieldToConceptIdMapping.shippingTrackingNumber]
+                          : "N/A"
+                      }</td>
+                      <td>${
+                        i[fieldToConceptIdMapping.shippingSite]
+                          ? i[fieldToConceptIdMapping.shippingSite]
+                          : "N/A"
+                      }</td>
+                      <td>${Object.keys(i.bags).length}</td>
+                      <td><button class="manifest-button" data-toggle="modal" data-target="#manifestModal">Manifest</button></td>
+                      </tr>`;
+      });
+      return template;
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
