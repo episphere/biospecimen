9023e858878584609ab2e80631c0dcf13c290fb6
Iqbal Singh <15388759+iqbal-singh@users.noreply.github.com>
2021-09-10 13:31:09 -0400

    change 'Labeling Receipt' to 'Labeling Scanning'

--- src/pages/explanation.js
+++ src/pages/explanation.js
@@ -92,7 +92,7 @@ export const explanationTemplate = (dt, biospecimenData) => {
                 </br>
                 <div class="form-group row">
                     <div class="col-auto">
-                        <button class="btn btn-outline-danger" data-connect-id="${dt.Connect_ID}" data-master-specimen-id="${biospecimenData['820476880']}" id="returnToCollectProcess">${getWorflow() === 'research' ? 'Return to Process' : 'Return to Labeling and Receipt'}</button>
+                        <button class="btn btn-outline-danger" data-connect-id="${dt.Connect_ID}" data-master-specimen-id="${biospecimenData['820476880']}" id="returnToCollectProcess">${getWorflow() === 'research' ? 'Return to Process' : 'Return to Labeling and Scanning'}</button>
                     </div>
                     <div class="ml-auto">
                         <button class="btn btn-outline-warning" data-connect-id="${dt.Connect_ID}" data-master-specimen-id="${biospecimenData['820476880']}" type="button" id="explanationSaveExit">Exit</button>
