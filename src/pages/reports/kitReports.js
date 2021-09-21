220d3020b48e550f113adbf5ea75ab44424b3d84
Gene Barra <Gebarra9@gmail.com>
2021-09-21 11:03:31 -0400

    add plotly quick setup from docs on kit reports page

--- src/pages/reports/kitReports.js
+++ src/pages/reports/kitReports.js
@@ -3,21 +3,53 @@ import { nonUserNavBar, unAuthorizedUser } from "../../navbar.js";
 import { activeKitReportsNavbar } from "./activeKitReportsNavbar.js";
 
 export const kitReportsScreen = async (auth, route) => {
-  const user = auth.currentUser;
-  if (!user) return;
-  const username = user.displayName ? user.displayName : user.email;
-  kitReportsTemplate(username, auth, route);
+    const user = auth.currentUser;
+    if (!user) return;
+    const username = user.displayName ? user.displayName : user.email;
+    kitReportsTemplate(username, auth, route);
+    plotly();
 };
 
 const kitReportsTemplate = async (username, auth, route) => {
-  let template = "";
+    let template = "";
 
-  template += kitReportsNavbar();
+    template += kitReportsNavbar();
 
-  template += `<h3>Reports Screen</h3>`;
+    template += `<h3>Reports Screen</h3>`;
+    template += `<div id="tester" style="width:600px;height:250px;"></div>
+  `;
 
-  document.getElementById("contentBody").innerHTML = template;
-  document.getElementById("navbarNavAltMarkup").innerHTML =
-    nonUserNavBar(username);
-  activeKitReportsNavbar();
+    document.getElementById("contentBody").innerHTML = template;
+    document.getElementById("navbarNavAltMarkup").innerHTML =
+        nonUserNavBar(username);
+    activeKitReportsNavbar();
+    // await testPlotly();
+};
+
+const plotly = () => {
+    const script = document.createElement("script");
+    script.setAttribute("src", "https://cdn.plot.ly/plotly-latest.min.js");
+    document.body.appendChild(script);
+
+    script.addEventListener("load", function () {
+        // Plotly loaded
+        console.log(Plotly);
+        testPlotly();
+    });
+};
+
+const testPlotly = () => {
+    const TESTER = document.getElementById("tester");
+    Plotly.newPlot(
+        TESTER,
+        [
+            {
+                x: [1, 2, 3, 4, 5],
+                y: [1, 2, 4, 8, 16],
+            },
+        ],
+        {
+            margin: { t: 0 },
+        }
+    );
 };
