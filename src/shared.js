d4fb39def231ae0e497cd99c17bb08d624b00c34
Gbarra9 <gebarra9@gmail.com>
2021-12-13 23:45:33 -0500

    change site contact info obj

--- src/shared.js
+++ src/shared.js
@@ -595,19 +595,6 @@ export const siteFullNames = {
 }
 
 export const siteContactInformation = {
-  "NCI1":[{
-    "fullName":"",
-    "email":"",
-    "phone":[""],
-  }],
-  "NCI":[{
-    "fullName":"Erin Schwartz",
-    "email":"Erin.C.Schwartz@HealthPartners.com",
-    "phone":[
-     "Office: (651) 495-6371",
-     "Cell: (612) 836-7885"
-    ]
-  }],
   "UCM":[{
     "fullName":"Jaime King",
     "email":"jaimeking@bsd.uchicago.edu",
