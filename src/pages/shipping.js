f104fad53e07c5beb6964de77debf75f526d1d44
Gbarra9 <gebarra9@gmail.com>
2022-01-10 17:19:12 -0500

    spell out temperature and add margin left to checkbox

--- src/pages/shipping.js
+++ src/pages/shipping.js
@@ -194,8 +194,8 @@ export const startShipping = async (userName) => {
     </div>
     <div class="row" id="checkForTemp">
         <div class="col-lg">
-            <input type="checkbox" id="tempMonitorChecked" style="transform: scale(1.5); margin-right:10px; margin-top:5px;">
-            <label for="tempMonitorChecked">Temp Monitor is included in this shipment</label><br>
+            <input type="checkbox" id="tempMonitorChecked" style="transform: scale(1.5); margin-right:10px; margin-top:5px; margin-left:5px;">
+            <label for="tempMonitorChecked">Temperature Monitor is included in this shipment</label><br>
         </div>
     </div>
     
