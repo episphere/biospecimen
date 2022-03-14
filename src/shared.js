622b7df51830332b10fc7e931d7b7e173b821e12
anthonypetersen <tonypetersen@hotmail.com>
2022-03-14 11:09:00 -0500

    updated email template

--- src/shared.js
+++ src/shared.js
@@ -1111,28 +1111,31 @@ export const checkedIn = (data) => {
 const baselineEmailTemplate = (data) => {
   
     return `
-        Dear ${data['399159511']}
-        <br>
-        <br>
+        Dear ${data['399159511']},
+        <br/>
+        <br/>
         Thank you for donating your samples for the Connect for Cancer Prevention Study! Next, please visit the <a href="https://myconnect.cancer.gov/#dashboard">MyConnect app</a> to answer the Baseline Blood, Urine, and Mouthwash Sample Survey. This short survey asks questions about the day that you donated samples, so it is important to complete it as soon as you can.
-        <br>
-        <br>
+        <br/>
+        <br/>
         Have questions? Please contact the <a href="https://myconnect.cancer.gov/support">Connect Support Center.</a>
-        <br>
-        <br>
+        <br/>
+        <br/>
         Thank you for your commitment to helping us learn how to better prevent cancer.
-        <br>
-        <br>
+        <br/>
+        <br/>
         Sincerely,
-        <br>
+        <br/>
         the Connect team at the National Cancer Institute
-        <br>
+        <br/>
         9609 Medical Center Drive, Rockville MD 20850
-        <br>
-        To protect your information, we follow federal privacy rules, including the Privacy Act and the Common Rule.
-        <br>
-        <br>
-        This message is private. If you have received it by mistake, please let us know by emailing ConnectSupport@NORC.org, and please kindly delete the message. If you are not the right recipient, please do not share this message with anyone.
+        <br/>
+        <img src="https://raw.githubusercontent.com/episphere/connectApp/master/images/new_logo.png" style="width:150px;height:40px;">
+        <br/>
+        <br/>
+        <em>To protect your information, we follow federal privacy rules, including the <a href="https://www.justice.gov/archives/opcl/overview-privacy-act-1974-2015-edition">Privacy Act</a> and the <a href="https://grants.nih.gov/grants/guide/notice-files/NOT-OD-19-050.html">Common Rule</a>.</em>
+        <br/>
+        <br/>
+        <em>This message is private. If you have received it by mistake, please let us know by emailing ConnectSupport@NORC.org, and please kindly delete the message. If you are not the right recipient, please do not share this message with anyone.</em>
     `;
 };
 
@@ -1188,7 +1191,7 @@ export const checkInParticipant = async (data, visitConcept) => {
         
     if(sendBioEmail) {
         const emailData = {
-            email: "tony.petersen@nih.gov",
+            email: "petersenaa@nih.gov",
             subject: "Please complete a short survey about your samples",
             message: baselineEmailTemplate(data)
         };
