55e89ac53d8a89b4cc8f1f2a131c7e41619ea38f
Warren Lu <warren.lu6@gmail.com>
2022-04-21 10:51:11 -0400

    Solve issues #442 and #459

--- src/pages/collectProcess.js
+++ src/pages/collectProcess.js
@@ -11,7 +11,7 @@ export const tubeCollectedTemplate = (data, formData) => {
         </br>
         <div class="row">
             <div class="col">
-                <div class="row">${data['996038075']}, ${data['399159511']}</div>
+                <div class="row"><h5>${data['996038075']}, ${data['399159511']}</h5></div>
                 <div class="row">Connect ID: <svg id="connectIdBarCode"></svg></div>
                 <div class="row">Collection ID: ${formData['820476880']}</div>
                 <div class="row">Collection ID Link Date/Time: ${new Date(formData['678166505']).toLocaleString()}</div>
