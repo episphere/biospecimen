bf0de2fb836d3e1b4d45a5b07118c1ef8e70791f
anthonypetersen <tonypetersen@hotmail.com>
2022-05-24 20:51:06 -0500

    adding routes to service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -34,6 +34,32 @@ registerRoute(
     'GET'
 );
 
+registerRoute(
+    new RegExp('https://api-myconnect-stage.cancer.gov/.+'),
+    new NetworkFirst({
+        cacheName: 'api-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            })
+        ]
+    }),
+    'GET'
+);
+
+registerRoute(
+    new RegExp('https://api-myconnect.cancer.gov/.+'),
+    new NetworkFirst({
+        cacheName: 'api-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            })
+        ]
+    }),
+    'GET'
+);
+
 const bgSyncPlugin = new BackgroundSyncPlugin('connectBiospecimen', {
     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
 });
@@ -45,4 +71,21 @@ registerRoute(
     }),
     'POST'
 );
+
+registerRoute(
+    new RegExp('https://api-myconnect-stage.cancer.gov/.+'),
+    new NetworkOnly({
+      plugins: [bgSyncPlugin]
+    }),
+    'POST'
+);
+
+registerRoute(
+    new RegExp('https://api-myconnect.cancer.gov/.+'),
+    new NetworkOnly({
+      plugins: [bgSyncPlugin]
+    }),
+    'POST'
+);
+
 workbox.precaching.precacheAndRoute([{url: 'index.html', revision: `1`}]);
