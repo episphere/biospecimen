4293a5441c52753d3d1a6ca80f5be5cc7966558b
geniosys <hise05@hotmail.com>
2022-08-04 01:21:55 -0400

    refactor specimenjs to include clinical specimen link

--- src/pages/specimen.js
+++ src/pages/specimen.js
@@ -14,7 +14,7 @@ export const specimenTemplate = async (data) => {
     formData['siteAcronym'] = getSiteAcronym();
     formData['827220437'] = parseInt(getSiteCode());
     let visit = visitType.filter(visit => visit.concept === getCheckedInVisit(data))[0];
-
+    const workflow = getWorflow() ?? localStorage.getItem('workflow');
 
     let template = `
         </br>
@@ -35,12 +35,12 @@ export const specimenTemplate = async (data) => {
         <div class="">
             <h4>Link a new Collection ID</h4><br/>
             <h4> Visit: ${visit.visitType}</h4>
-        </div>
-
-        <form id="specimenLinkForm" method="POST" data-participant-token="${data.token}" data-connect-id="${data.Connect_ID}">
-            <div class="form-group row">`
+        </div>`
+        template += `<form id="specimenLinkForm" method="POST" data-participant-token="${data.token}" data-connect-id="${data.Connect_ID}">`;
+        if(workflow === 'research') {
+        template = `<div class="form-group row">`
                 const siteAcronym = getSiteAcronym();
-                const workflow = getWorflow() ?? localStorage.getItem('workflow');
+                
                 if(siteLocations[workflow] && siteLocations[workflow][siteAcronym]) {
                     template +=`<label class="col-md-4 col-form-label" for="collectionLocation">Select Collection Location</label>
                     <select class="form-control col-md-5" id="collectionLocation">
@@ -51,20 +51,6 @@ export const specimenTemplate = async (data) => {
                     template +=`</select>`
                 }
             template +=`</div>`
-            if(workflow === 'clinical') {
-                template += `
-                    <div class="form-group row">
-                        <label class="col-md-4 col-form-label" for="accessionID1">Scan in Accession ID:</label>
-                        <input autocomplete="off" type="text" class="form-control col-md-5" ${siteAcronym === 'KPCO' || siteAcronym === 'KPGA' || siteAcronym === 'KPNW' || siteAcronym === 'KPHI' ? 'required': ''} placeholder="Scan/Type in Accession ID from Tube" id="accessionID1"/>
-                        
-                        <button class="barcode-input-clear" hidden="true" type="button" id="clearScanAccessionID" title="Clear scanned barcode" data-enable-input="accessionID2" data-barcode-input="accessionID1"><i class="fas fa-times"></i></button>
-                    </div>
-                    <div class="form-group row">
-                        <input autocomplete="off" type="text" class="form-control col-md-5 offset-4" placeholder="Re-Type in Accession ID from Tube" id="accessionID2"/>
-                    </div>
-                    </br>
-                `
-            }
             template += `<div class="form-group row">
                 <label class="col-md-4 col-form-label" for="scanSpecimenID">Scan Collection ID from Label Sheet Label</label>
                 <input autocomplete="off" type="text" class="form-control col-md-5" placeholder="Scan in Collection ID from Label Sheet Label" id="scanSpecimenID"/> 
@@ -83,17 +69,58 @@ export const specimenTemplate = async (data) => {
                 <div class="col">
                     <button class="btn btn-outline-primary float-right" data-connect-id="${data.Connect_ID}" type="submit" id="specimenContinue">Submit</button>
                 </div>
+            </div>`;
+     } else {
+        template += `<div class="form-group row">`
+        const siteAcronym = getSiteAcronym();
+        template += `<select class="custom-select" id="visit-select">
+                                <option value=""> -- Select Visit -- </option>`;
+                                
+                Array.from(visitType).forEach(option => {
+                    template += option.visitType === "Baseline" ? `<option value=${option.concept}>${option.visitType}</option>` : `<option value=${option.concept} disabled>${option.visitType}</option>`;
+                })
+
+                template += `</select>`;
+    template +=`</div>`
+        template += `
+            <div class="form-group row">
+                <label class="col-md-4 col-form-label" for="accessionID1">Scan Blood Accession ID:</label>
+                <input autocomplete="off" type="text" class="form-control col-md-5" ${siteAcronym === 'KPCO' || siteAcronym === 'KPGA' || siteAcronym === 'KPNW' || siteAcronym === 'KPHI' ? 'required': ''} placeholder="Scan/Type in Accession ID from Tube" id="accessionID1"/>
+                
+                <button class="barcode-input-clear" hidden="true" type="button" id="clearScanAccessionID" title="Clear scanned barcode" data-enable-input="accessionID2" data-barcode-input="accessionID1"><i class="fas fa-times"></i></button>
             </div>
-        </form>
-        </br>
-    `;
+            <div class="form-group row">
+                <input autocomplete="off" type="text" class="form-control col-md-5 offset-4" placeholder="Re-Type in Accession ID from Tube" id="accessionID2"/>
+            </div>
+            </br>
+            <div class="form-group row">
+                <label class="col-md-4 col-form-label" for="accessionID3">Scan Urine Accession ID:</label>
+                <input autocomplete="off" type="text" class="form-control col-md-5" ${siteAcronym === 'KPCO' || siteAcronym === 'KPGA' || siteAcronym === 'KPNW' || siteAcronym === 'KPHI' ? 'required': ''} placeholder="Scan/Type in Accession ID from Tube" id="accessionID3"/>
+                
+                <button class="barcode-input-clear" hidden="true" type="button" id="clearScanAccessionID" title="Clear scanned barcode" data-enable-input="accessionID4" data-barcode-input="accessionID3"><i class="fas fa-times"></i></button>
+            </div>
+            <div class="form-group row">
+                <input autocomplete="off" type="text" class="form-control col-md-5 offset-4" placeholder="Re-Type in Accession ID from Tube" id="accessionID4"/>
+            </div>
+            
+    <div class="form-group row">
+        <div class="col">
+            <button class="btn btn-outline-primary float-right" data-connect-id="${data.Connect_ID}" type="submit" id="clinicalSpecimenContinue">Submit</button>
+        </div>
+    </div>`
+    }
+    template += `</form>
+    </br>`;
     
     document.getElementById('contentBody').innerHTML = template;
     document.getElementById('enterSpecimenID2').onpaste = e => e.preventDefault();
+    document.getElementById('accessionID2').onpaste = e => e.preventDefault();
+    document.getElementById('accessionID4').onpaste = e => e.preventDefault();
     // addEventBarCodeScanner('scanSpecimenIDBarCodeBtn', 0, masterSpecimenIDRequirement.length);
     // if(document.getElementById('scanAccessionIDBarCodeBtn')) addEventBarCodeScanner('scanAccessionIDBarCodeBtn');
     generateBarCode('connectIdBarCode', data.Connect_ID);
     addEventSpecimenLinkForm(formData);
+    addEventClinicalSpecimenLinkForm(formData);
     addEventBackToSearch('navBarSearch');
     addEventNavBarParticipantCheckIn();
 }
