9034a9cac66ee08b14ef41d382c9dc10e1d5757b
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2022-11-10 10:59:02 -0500

    aded test location propety:wq

--- src/shared.js
+++ src/shared.js
@@ -1186,6 +1186,7 @@ export const siteSpecificLocation = {
   "DCAM": {"siteAcronym":"UCM", "siteCode":809703864, "loginSiteName": "University of Chicago Medicine"},
   "Main Campus": {"siteAcronym":"NIH", "siteCode":13, "loginSiteName": "National Cancer Institute"},
   "Frederick": {"siteAcronym":"NIH", "siteCode":13, "loginSiteName": "National Cancer Institute"},
+  "Rockville": {"siteAcronym":"NIH", "siteCode":13, "loginSiteName": "National Cancer Institute"},
 }
 
 export const locationConceptIDToLocationMap = {
