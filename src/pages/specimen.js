b3a0df309d4d898a33d8459e3b43ac7dff061b79
anthonypetersen <tonypetersen@hotmail.com>
2022-11-29 14:31:31 -0600

    cleaning up and cutting off end of barcode value

--- src/pages/specimen.js
+++ src/pages/specimen.js
@@ -36,31 +36,44 @@ export const specimenTemplate = async (data, formData) => {
 
         </br>`
 
-        template += `<form id="specimenLinkForm" method="POST" data-participant-token="${data.token}" data-connect-id="${data.Connect_ID}">`;
-        if(workflow === 'research') {
-            let visit = visitType.filter(visit => visit.concept === getCheckedInVisit(data))[0];
-            template += `<div class="">
-                            <h4>Link a new Collection ID</h4><br/>
-                            <h4> Visit: ${visit.visitType}</h4>
-                        </div>`;
-        template += `<div class="form-group row">`
-                const siteAcronym = getSiteAcronym();
+    template += `<form id="specimenLinkForm" method="POST" data-participant-token="${data.token}" data-connect-id="${data.Connect_ID}">`;
+        
+    if(workflow === 'research') {
+        let visit = visitType.filter(visit => visit.concept === getCheckedInVisit(data))[0];
+            
+        template += `
+            <div class="">
+                <h4>Link a new Collection ID</h4><br/>
+                <h4> Visit: ${visit.visitType}</h4>
+            </div>
+            
+            <div class="form-group row">`
                 
-                if(siteLocations[workflow] && siteLocations[workflow][siteAcronym]) {
-                    template +=`<label class="col-md-4 col-form-label" for="collectionLocation">Select Collection Location</label>
-                    <select class="form-control col-md-5" id="collectionLocation">
+        const siteAcronym = getSiteAcronym();
+                
+        if(siteLocations[workflow] && siteLocations[workflow][siteAcronym]) {
+            template += `
+                <label class="col-md-4 col-form-label" for="collectionLocation">Select Collection Location</label>
+                <select class="form-control col-md-5" id="collectionLocation">
                     <option value='none'>Please Select Location</option>`
-                    siteLocations[workflow][siteAcronym].forEach(site => {
-                        template +=`<option ${locationSelection === site.concept.toString() ? 'selected="selected"' : ""} value='${site.concept}'>${site.location}</option>`
-                    })
-                    template +=`</select>`
-                }
-            template +=`</div>`
-            template += `<div class="form-group row">
+
+            siteLocations[workflow][siteAcronym].forEach(site => {
+                template += `<option ${locationSelection === site.concept.toString() ? 'selected="selected"' : ""} value='${site.concept}'>${site.location}</option>`
+            });
+
+            template += `
+                </select>`
+        }
+            
+        template += `
+            </div>
+            <div class="form-group row">
                 <label class="col-md-4 col-form-label" for="scanSpecimenID">Scan/Type Collection ID from Label Sheet</label>
                 <input autocomplete="off" type="text" class="form-control col-md-5" placeholder="Scan/Type Collection ID from Label Sheet" id="scanSpecimenID" data-isscanned="false" /> 
             </div>
+
             </br>
+
             <div class="form-group row">
                 <label class="col-md-4 col-form-label" for="scanSpecimenID2">Re-Scan/Type Collection ID from Label Sheet</label>
                 <input autocomplete="off" type="text" class="form-control col-md-5" placeholder="Re-Scan/Type Collection ID from Label Sheet" id="scanSpecimenID2" data-isscanned="false" />
@@ -72,7 +85,8 @@ export const specimenTemplate = async (data, formData) => {
                 </div>
             </div>`;
 
-     } else if(isSpecimenLinkForm2) {// clinical specimen page 2
+    } 
+    else if(isSpecimenLinkForm2) {// clinical specimen page 2
         let visit = visitType.filter(visit => visit.concept === formData['331584571'].toString())[0];
             template += `<div class="row">
                             <div class="column">
@@ -98,7 +112,8 @@ export const specimenTemplate = async (data, formData) => {
                                 <button class="btn btn-outline-primary float-right" data-connect-id="${data.Connect_ID}" type="submit" id="specimenContinue">Submit</button>
                             </div>
                         </div>`
-     } else  {// clinical specimen page 1
+    } 
+    else {// clinical specimen page 1
         template += `<div class="form-group row">`
         const siteAcronym = getSiteAcronym();
         template += `<select class="custom-select" id="visit-select">
@@ -132,35 +147,43 @@ export const specimenTemplate = async (data, formData) => {
                 <input autocomplete="off" type="text" class="form-control col-md-5 offset-4" placeholder="Re-Enter (scan/type) in Accession ID from Urine Tube" id="accessionID4"/>
             </div>
             
-    <div class="form-group row">
-        <div class="col">
-            <button class="btn btn-outline-primary float-right" data-connect-id="${data.Connect_ID}" type="submit" id="clinicalSpecimenContinue">Submit</button>
-        </div>
-    </div>`
+        <div class="form-group row">
+            <div class="col">
+                <button class="btn btn-outline-primary float-right" data-connect-id="${data.Connect_ID}" type="submit" id="clinicalSpecimenContinue">Submit</button>
+            </div>
+        </div>`
     }
-    template += `</form>
-    </br>`;
+
+    template += `
+        </form>
+        </br>`;
+
     document.getElementById('contentBody').innerHTML = template;
-    //JS Events logic 
+     
     if(workflow === 'research') {
         document.getElementById('scanSpecimenID2').onpaste = e => e.preventDefault();
-        addEventSpecimenLinkForm(formData);
-        addSelectionEventListener("collectionLocation", "specimenLink_location");
         
-    } else if (isSpecimenLinkForm2) {// clinical specimen page 2
-        document.getElementById('scanSpecimenID2').onpaste = e => e.preventDefault();
-        addEventClinicalSpecimenLinkForm2(formData);
+        addSelectionEventListener("collectionLocation", "specimenLink_location");
         collectionInputValidator(['scanSpecimenID', 'scanSpecimenID2']);
 
-    } else {//clinical specimen page 1
+        addEventSpecimenLinkForm(formData);
+    } 
+    else if (isSpecimenLinkForm2) {// clinical specimen page 2
+        document.getElementById('scanSpecimenID2').onpaste = e => e.preventDefault();
+
+        collectionInputValidator(['scanSpecimenID', 'scanSpecimenID2']);
+
+        addEventClinicalSpecimenLinkForm2(formData);
+    } 
+    else {//clinical specimen page 1
         document.getElementById('accessionID2').onpaste = e => e.preventDefault();
         document.getElementById('accessionID4').onpaste = e => e.preventDefault();
-        addEventClinicalSpecimenLinkForm(formData);
-        numericInputValidator(['accessionID1', 'accessionID2', 'accessionID3', 'accessionID4']);
- }
 
-    // addEventBarCodeScanner('scanSpecimenIDBarCodeBtn', 0, masterSpecimenIDRequirement.length);
-    // if(document.getElementById('scanAccessionIDBarCodeBtn')) addEventBarCodeScanner('scanAccessionIDBarCodeBtn');
+        numericInputValidator(['accessionID1', 'accessionID2', 'accessionID3', 'accessionID4']);
+
+        addEventClinicalSpecimenLinkForm(formData);
+    }
+
     generateBarCode('connectIdBarCode', data.Connect_ID);
     addEventBackToSearch('navBarSearch');
     addEventNavBarParticipantCheckIn();
