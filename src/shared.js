95b4eb426cef9c9ed5a772b729886373a99888f2
Gbarra9 <gebarra9@gmail.com>
2023-03-21 14:30:28 -0400

    dynamically create packages in transit table, setup function to dynamically create packages in tranist modal table, add some styling to table text using external css

--- src/shared.js
+++ src/shared.js
@@ -833,6 +833,10 @@ export const removeBag = async(boxId, bags) => {
     
 }
 
+/**
+ * Fetches biospecimen collection data from the database
+ * @returns {Promise}
+ */
 export const searchSpecimenInstitute = async() => { // rename to searchSpecimenInstitute
     const idToken = await getIdToken();
     const response = await fetch(`${api}api=searchSpecimen`, {
