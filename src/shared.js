2d8abe7c164383ddbee1c8dc40799d4e29096c7b
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2023-08-16 16:29:43 -0400

    removed local host api

--- src/shared.js
+++ src/shared.js
@@ -118,7 +118,7 @@ export const findParticipant = async (query) => {
 
 export const getDailyParticipant = async (query) => {
   const idToken = await getIdToken();
-  const response = await fetch(`http://127.0.0.1:5001/nih-nci-dceg-connect-dev/us-central1/biospecimen?api=getDailyReportParticipants`, {
+  const response = await fetch(`${api}api=getDailyReportParticipants`, {
       method: "GET",
       headers: {
           Authorization:"Bearer "+idToken
