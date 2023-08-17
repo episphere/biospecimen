0fb160b159e9d09cf0ad9fbaf9b48c24a332ba7a
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2023-08-17 11:54:12 -0400

    removed localhost

--- src/pages/receipts/packageReceipt.js
+++ src/pages/receipts/packageReceipt.js
@@ -305,7 +305,7 @@ const storeDateReceivedinISO = (date) => { // ("YYYY-MM-DD" to ISO format DateTi
 const storePackageReceipt = async (data) => {
     showAnimation();
     const idToken = await getIdToken();
-    const response = await fetch(`http://127.0.0.1:5001/nih-nci-dceg-connect-dev/us-central1/biospecimen?api=storeReceipt`,
+    const response = await fetch(`${baseAPI}api=storeReceipt`,
         {
             method: "POST",
             body: JSON.stringify(data),
