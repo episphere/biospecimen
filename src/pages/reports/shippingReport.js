9fefab6d534580a2231faa5d7bd1ba23a09a071a
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2023-09-26 11:48:02 -0400

    feedback implemented

--- src/pages/reports/shippingReport.js
+++ src/pages/reports/shippingReport.js
@@ -10,23 +10,20 @@ import { startReport } from "../reportsQuery.js";
 export const bptlShipReportsScreen = async (auth, route) => {
     const user = auth.currentUser;
     if (!user) return;
-    const username = user.displayName ? user.displayName : user.email;
-    appState.setState({ 'username': username });
+    const username = user.displayName || user.email;
+    appState.setState({ username });
     bptlShipReportsScreenTemplate(username);
 };
 
 export const bptlShipReportsScreenTemplate = async (username) => {
-    let template = "";
-    template += reportsNavbar();
-    template += ` 
-              <div id="root root-margin" style="padding-top: 25px;">
-                <h3 style="text-align: center; margin: 1rem 0;">Shipping Report Screen</h3>
-                    <div>
-                        ${startReport('bptlShippingReport')}
-                    </div>
-              </div>
-
-  `;
+    const template = ` ${reportsNavbar()}
+        <div id="root root-margin" style="padding-top: 25px;">
+        <h3 style="text-align: center; margin: 1rem 0;">Shipping Report Screen</h3>
+            <div>
+                ${startReport('bptlShippingReport')}
+            </div>
+        </div>
+    `;
 
     document.getElementById("contentBody").innerHTML = template;
     document.getElementById("navbarNavAltMarkup").innerHTML = nonUserNavBar(username);
