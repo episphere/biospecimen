bf505211c88bba8e89ab8a937aa5b1767d701934
ramavavi <vaishuramavarapu@gmail.com>
2024-01-10 17:30:48 -0500

    added space after if and adding the strict equality operator

--- src/pages/homeCollection/kitAssembly.js
+++ src/pages/homeCollection/kitAssembly.js
@@ -110,14 +110,14 @@ const enableEnterKeystroke = () => {
 
 const performQCcheck = (inputBox2, inputBox1, errorTag, errorMsg) => {
   const checkInputBox2 = document.getElementById(inputBox2);
-  if(errorMsg == "") {
+  if (errorMsg == "") {
     document.getElementById(errorTag).innerHTML = `` 
   }
   if (checkInputBox2) {
     checkInputBox2.addEventListener("input", (e) => {
       const checkInputBox2Value = e.target.value.trim();
       const checkInputBox1Value = document.getElementById(inputBox1).value.trim();
-      if (checkInputBox2Value != checkInputBox1Value) {
+      if (checkInputBox2Value !== checkInputBox1Value) {
         document.getElementById(errorTag).innerHTML = `<i class="fa fa-exclamation-circle" style="font-size: 14px; color: red;"></i> ${errorMsg}`
       }
       else {
