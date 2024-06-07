4af785a0db65dd9f9d536aa0e62f9461863ab76c
Gene Barra <gebarra9@gmail.com>
2024-06-07 16:28:41 -0400

    add to SSOConfig obj

--- src/prod/identityProvider.js
+++ src/prod/identityProvider.js
@@ -1,35 +1,40 @@
 export const prodSSOConfig = (tenantID, provider, inputValue) => {
-    if(/nih.gov/i.test(inputValue)) {
+    if (/nih.gov/i.test(inputValue)) {
         tenantID = 'NIH-SSO-wthvn';
         provider = 'saml.nih-sso';
     };
-    if(/healthpartners.com/i.test(inputValue)) {
+    if (/healthpartners.com/i.test(inputValue)) {
         tenantID = 'HP-SSO-252sf';
         provider = 'saml.healthpartner';
-    };
-    if(/sanfordhealth.org/i.test(inputValue)) {
+    }
+    if (/sanfordhealth.org/i.test(inputValue)) {
         tenantID = 'SFH-SSO-pb390';
         provider = 'saml.connect-sanford';
-    };
-    if(/norc.org/i.test(inputValue)) {
+    }
+    if (/norc.org/i.test(inputValue)) {
         tenantID = 'NORC-SSO-nwvau';
         provider = 'saml.connect-norc-prod';
-    };
-    if(/kp.org/i.test(inputValue)) {
+    }
+    if (/kp.org/i.test(inputValue)) {
         tenantID = 'KP-SSO-ii9sr';
         provider = 'saml.connect-kp';
-    };
-    if(/uchicago.edu/i.test(inputValue)) {
+    }
+    if (/uchicago.edu/i.test(inputValue)) {
         tenantID = 'UCM-SSO-p4f5m';
         provider = 'saml.connect-uchicago';
-    };
-    if(/hfhs.org/i.test(inputValue)) {
+    }
+    if (/hfhs.org/i.test(inputValue)) {
         tenantID = 'HFHS-SSO-lo99j';
         provider = 'saml.connect-hfhs';
-    };
-    if(/marshfieldresearch.org/i.test(inputValue) || /marshfieldclinic.org/i.test(inputValue)) {
+    }
+    if (/marshfieldresearch.org/i.test(inputValue) || /marshfieldclinic.org/i.test(inputValue)) {
         tenantID = 'MFC-SSO-tdj17';
         provider = 'saml.connect-mfc'
-    };
+    }
+    if (/bswhealth.org/i.test(inputValue)) {
+        tenantID = 'BSWH-SSO-dcoos';
+        provider = 'saml.connect-bswh';
+    }
+
     return {tenantID, provider}
 }
--- src/stage/identityProvider.js
+++ src/stage/identityProvider.js
@@ -1,35 +1,40 @@
 export const stageSSOConfig = (tenantID, provider, inputValue) => {
-    if(/nih.gov/i.test(inputValue)) {
+    if (/nih.gov/i.test(inputValue)) {
         tenantID = 'NIH-SSO-9q2ao';
         provider = 'saml.nih-sso';
-    };
-    if(/healthpartners.com/i.test(inputValue)) {
+    }
+    if (/healthpartners.com/i.test(inputValue)) {
         tenantID = 'HP-SSO-1elez';
         provider = 'saml.healthpartner';
-    };
-    if(/sanfordhealth.org/i.test(inputValue)) {
+    }
+    if (/sanfordhealth.org/i.test(inputValue)) {
         tenantID = 'SFH-SSO-uetfo';
         provider = 'saml.connect-sanford';
-    };
-    if(/norc.org/i.test(inputValue)) {
+    }
+    if (/norc.org/i.test(inputValue)) {
         tenantID = 'NORC-SSO-l80az';
         provider = 'saml.connect-norc';
-    };
-    if(/kp.org/i.test(inputValue)) {
+    }
+    if (/kp.org/i.test(inputValue)) {
         tenantID = 'KP-SSO-ssj7c';
         provider = 'saml.connect-kp';
-    };
-    if(/uchicago.edu/i.test(inputValue)) {
+    }
+    if (/uchicago.edu/i.test(inputValue)) {
         tenantID = 'UCM-SSO-lrjsp';
         provider = 'saml.connect-uchicago';
-    };
-    if(/hfhs.org/i.test(inputValue)) {
+    }
+    if (/hfhs.org/i.test(inputValue)) {
         tenantID = 'HFHS-SSO-eq1fj';
         provider = 'saml.connect-hfhs';
-    };
-    if(/marshfieldresearch.org/i.test(inputValue) || /marshfieldclinic.org/i.test(inputValue)) {
+    }
+    if (/marshfieldresearch.org/i.test(inputValue) || /marshfieldclinic.org/i.test(inputValue)) {
         tenantID = 'MFC-SSO-6x4zy';
         provider = 'saml.connect-mfc'
-    };
+    }
+    if (/bswhealth.org/i.test(inputValue)) {
+        tenantID = 'BSWH-SSO-k4cat';
+        provider = 'saml.connect-bswh';
+    }
+
     return {tenantID, provider}
 }
