7287f162cb27904f5ee37d7948eef8c1e38b4399
amber-emmes <ablaylock@emmes.com>
2024-06-14 12:17:59 -0400

    Addressed PR comments

--- src/pages/homeCollection/kitCSV.js
+++ src/pages/homeCollection/kitCSV.js
@@ -36,9 +36,7 @@ const csvFileButtonSubmit = () => {
       showAnimation();
       try {
           const results = await getKitsByReceivedDate(dateString);
-          console.log('results', results);
           const modifiedResults = modifyKitQueryResults(results.data);
-          console.log('modifiedResults', modifiedResults);
           generateKitCSVData(modifiedResults);
           hideAnimation();
       } catch (e) {
@@ -113,39 +111,8 @@ const modifyKitQueryResults = (kitsData) => {
 
 const getVialTypesMapping = (collectionType, site, tubeId) => {
   if (collectionType === 'home') {
-    return vialMapping[collectionType]?.[site]?.[tubeId] || ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"];
-  }
-}
-
-const vialMapping = {
-  'home': {
-    'henryFordHealth': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'healthPartners': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'kpCO':{
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'kpGA': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'kpHI': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'kpNW': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'sanfordHealth': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'uOfChicagoMed': {
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    },
-    'nci':{  // testing only
-      '0007' : ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"]
-    }
+    // Home collections, currently the only type used, always return the same value
+    return ["15ml Nalgene jar",	"Crest Alcohol Free",	"Saliva",	"10"];
   }
 }
 
